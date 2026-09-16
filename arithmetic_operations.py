dd20782d474b2caa6e9b270b297da877cde9b0f6
delvicario.angeli <delvicario.angeli@student.ccm.edu>
2026-09-16 11:16:39 -0400

    Completed Week 4 Lab 03 Assignment

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -6,7 +6,8 @@
 # 9/16/26
 
 
-
+print (" Basic Arithmetic Calculatior!")
+print ("=======================================")
 #ask user for two numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
@@ -19,6 +20,7 @@ power = (num1 ** num2)
 average = (num1 + num2) /2
 
 #print all six results
+print("__Results__")
 print ("Addition: ", add )
 print ("Subtraction: " , sub)
 print ("Multiplication: " ,multiply)
